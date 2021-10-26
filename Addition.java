261b0820a31f61dd4ad01e01674cee8b8e9ba43f
AvinashAware <93067399+AvinashAware@users.noreply.github.com>
2021-10-26 18:25:04 +0530

    Add files via upload
    2 new added

---
+++ Addition.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Addition
+{
+public static void main(String[] args) 
+   {
+    int a,b,c;
+ Scanner s = new Scanner(System.in);
+ System.out.println("Enter a and b");
+ a=s.nextInt();
+ b=s.nextInt();
+ c=a-b;
+ System.out.println("Subtraction="+c);
+    
+    }
+}
